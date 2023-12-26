fd4b42118fd514c986476e15509a7137dc7b72f8
Chris Heaney <chrisheaney30@gmail.com>
2023-12-26 08:54:30 -0600

    prettify

--- tests/driftArchive.ts
+++ tests/driftArchive.ts
@@ -1,46 +1,49 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { DriftArchive } from "../target/types/drift_archive";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { DriftArchive } from '../target/types/drift_archive';
 
-import {
-  BN,
-  AdminClient,
-} from '@drift-labs/sdk';
+import { BN, AdminClient } from '@drift-labs/sdk';
 
 import { Keypair } from '@solana/web3.js';
-import {initializeQuoteSpotMarket, mockUSDCMint, mockUserUSDCAccount} from "./testHelpers";
+import {
+	initializeQuoteSpotMarket,
+	mockUSDCMint,
+	mockUserUSDCAccount,
+} from './testHelpers';
 
-describe("drift-archive", () => {
-  // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.local(undefined, {
-    preflightCommitment: 'confirmed',
-    skipPreflight: false,
-    commitment: 'confirmed',
-  });
+describe('drift-archive', () => {
+	// Configure the client to use the local cluster.
+	const provider = anchor.AnchorProvider.local(undefined, {
+		preflightCommitment: 'confirmed',
+		skipPreflight: false,
+		commitment: 'confirmed',
+	});
 
-  anchor.setProvider(provider);
+	anchor.setProvider(provider);
 
-  const adminClient = new AdminClient({
-    connection: provider.connection,
-    wallet: provider.wallet,
-  });
+	const adminClient = new AdminClient({
+		connection: provider.connection,
+		wallet: provider.wallet,
+	});
 
-  const program = anchor.workspace.DriftArchive as Program<DriftArchive>;
+	const program = anchor.workspace.DriftArchive as Program<DriftArchive>;
 
-  let usdcMint: Keypair;
-  let userUSDCAccount: Keypair;
-  const usdcAmount = new BN(1000 * 10 ** 6);
+	let usdcMint: Keypair;
+	let userUSDCAccount: Keypair;
+	const usdcAmount = new BN(1000 * 10 ** 6);
 
-  before(async () => {
-    usdcMint = await mockUSDCMint(provider);
-    userUSDCAccount = await mockUserUSDCAccount(usdcMint, usdcAmount, provider);
-    await adminClient.initialize(usdcMint.publicKey, false);
-    await adminClient.subscribe();
-    await adminClient.initializeUserAccount();
-    await initializeQuoteSpotMarket(adminClient, usdcMint.publicKey);
-  });
+	before(async () => {
+		usdcMint = await mockUSDCMint(provider);
+		userUSDCAccount = await mockUserUSDCAccount(usdcMint, usdcAmount, provider);
+		await adminClient.initialize(usdcMint.publicKey, false);
+		await adminClient.subscribe();
+		await adminClient.initializeUserAccount();
+		await initializeQuoteSpotMarket(adminClient, usdcMint.publicKey);
+	});
 
-  after(async () => {
-    await adminClient.unsubscribe();
-  });
+	it('should initialize the program', async () => {});
+
+	after(async () => {
+		await adminClient.unsubscribe();
+	});
 });
